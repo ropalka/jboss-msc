6c221d5c505ec8251b68226d9ea7a2a7988a895a
Richard Opálka <opalka.richard@gmail.com>
2021-07-08 19:09:41 +0200

    Eliminating useless ServiceMode.in() method

--- src/main/java/org/jboss/msc/service/ServiceMode.java
+++ src/main/java/org/jboss/msc/service/ServiceMode.java
@@ -4,7 +4,6 @@ package org.jboss.msc.service;
  * The controller mode for a service.
  */
 public enum ServiceMode {
-
     /**
      * Remove this service and all of its dependents.
      */
@@ -26,20 +25,4 @@ public enum ServiceMode {
      * Demand to start, recursively demanding dependencies.  This is the default mode.
      */
     ACTIVE,
-    ;
-
-    /**
-     * Determine if this mode is one of the given modes.
-     *
-     * @param modes the modes to check
-     * @return {@code true} if this mode is in the set; {@code false} otherwise
-     */
-    public boolean in(ServiceMode... modes) {
-        for (ServiceMode test : modes) {
-            if (this == test) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
